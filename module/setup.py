d3109d2da387dfc699fb5987b4ebf35885617f1c
levyxx <levy.b.j.s@gmail.com>
2020-04-20 12:24:59 -0400

    Wrote setup.py

---
+++ module/setup.py
@@ -0,0 +1,9 @@
+import setuptools as st
+
+
+st.setup(name='seq2cite',
+         version='0.1',
+         packages=st.find_packages(),
+         author="Benjamn Levy, Mark Lock",
+         url='https://github.com/NationalLimerickProductions/seq2cite'
+         )
