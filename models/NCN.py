1fd767e51f84e7b7783b80ad952a21576f06f92f
jsbachsonata <mark.y.lock@gmail.com>
2020-04-24 16:53:46 -0400

    first commit of neural citation network

---
+++ models/NCN.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+# encoder
+
+
+# decoder
+
+# encoder decoder
+
+# worry about data pipeline later
